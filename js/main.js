995f684ecb95dc440dd8251138288a7cfd3b21b2
Alec <a_riddick@fanshaweonline.ca>
2019-07-16 17:50:14 -0400

    Scrolling Images With Click and Isolated Banners

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@
 	const sigils = document.querySelectorAll('.sigilContainer'),
 		  lightBox = document.querySelector('.lightbox'),
 		  closeButton = document.querySelector('.close-lightbox'),
-		  houseVideo = document.querySelector('.house-stark');
+		  houseVideo = document.querySelector('.house-stark'),
+		  bannerImages = document.querySelector('#houseImages');
 
 	function popLightBox() {
 		// make the lightbox show up
@@ -25,7 +26,24 @@
 
 	}
 
-	sigils.forEach(sigil => sigil.addEventListener("click", popLightBox));
+	function animateBanners() {
+		// we need am offset that we can multiply by 
+		// to animate our banners to the left and make the active one show up
+
+		let offset = 600,
+			multiplier = this.dataset.offset; 
+			// this is the data-offset customer data attribute (class) on each of the sigils
+			console.log((offset * multiplier) + "px");
+
+		// move the banners to the right using our math from above
+		bannerImages.style.right = `${offset * multiplier + "px"}`;
+
+	}
+
+	// sigils.forEach(sigil => sigil.addEventListener("click", popLightBox));
+	sigils.forEach(sigil => sigil.addEventListener("click", animateBanners));
+
+
 	closeButton.addEventListener("click", closeLightBox);
 
 	houseVideo.addEventListener('ended', closeLightBox);
